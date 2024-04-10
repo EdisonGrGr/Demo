186e3a138330fd836e21f392012c63a506636028
EdisonGrGr <112044450+EdisonGrGr@users.noreply.github.com>
2024-04-09 19:53:22 -0500

    Segundo cambio

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hola kjbdfYooo%s!", name);
+      return String.format("Hola ASndreeee%s!", name);
     }
 }
